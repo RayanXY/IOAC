2a9d318fbaa78dd3bfa92f01385b6820d770ddb3
Rayan Avelino <rayanodb@gmail.com>
2018-06-07 14:47:21 -0300

    Cache <v2.0> Error: Segmentation failure after 'Read'

--- Cache/include/cache.h
+++ Cache/include/cache.h
@@ -2,10 +2,177 @@
 #define _CACHE_H_
 
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
 
-void readConfiguration ();
+class Memory {
+
+	public:
+		int wordsPerBlocks, memorySize, mapping, setSize, replacementPolicy;
+		std::vector< std::vector<int> > elements;
+
+		void createMemory (Memory &memory) {
+
+			std::vector<int> aux;
+			for (int i = 0; i < (memory.wordsPerBlocks * memory.memorySize); i++){
+				for (int j = 0; j < memory.wordsPerBlocks; j++, i++){
+					aux.push_back (i);
+				}
+				memory.elements.push_back (aux);
+				aux.clear ();
+				i--;
+			}
+
+		}
+
+		void showMemory (Memory &memory) {
+
+			std::cout << ">>> MEMÓRIA PRINCIPAL" << std::endl;
+			std::cout << "Bloco" << "\t" << "Endereço" << std::endl;
+
+			for (size_t i = 0; i < memory.elements.size (); i++){
+				for (int j = 0; j < memory.wordsPerBlocks; j++){
+					std::cout << i << "\t" << memory.elements[i][j] << std::endl;
+				}
+				std::cout << std::endl;
+			}
+
+		}
+	
+};
+
+
+class Cache : public Memory {
+
+	public:
+		int lines;
+		std::vector< std::vector<int> > elements;
+		std::vector<int> count;
+
+		void createCache (Cache &cache) {
+
+			std::vector<int> aux;
+
+			for (int i = 0; i < (cache.wordsPerBlocks * cache.lines); i++){
+				for (int j = 0; j < cache.wordsPerBlocks; j++, i++){
+					aux.push_back (-1);
+				}
+				cache.elements.push_back (aux);
+				aux.clear ();
+				i--;
+			}
+
+		}
+
+		void showCache (Cache &cache, Memory &memory) {
+
+			std::cout << "CACHE L1" << std::endl;
+			std::cout << "Linha" << "\t" << "Bloco" << "\t" << "Endereço" << std::endl;
+
+			for (size_t i = 0; i < cache.elements.size (); i++){
+				for (int j = 0; j < cache.wordsPerBlocks; j++){
+					std::cout << i << "\t" << cache.elements[i][j]/memory.wordsPerBlocks << "\t" << cache.elements[i][j] << std::endl;
+				}
+				std::cout << std::endl;
+			}
+
+		}
+
+		int findLeastFrequentlyUsed (Cache &cache){
+
+			int less = cache.count[0];
+			int lessIndex = 0;
+
+			for (int i = 1; i < cache.lines; i++){
+				if (cache.count[i] < less){
+					less = cache.count[i];
+					lessIndex = i;
+				}
+			}
+
+			return lessIndex;
+
+		}
+
+		int findLeastFrequentlyUsedSets (Cache &cache, int begin, int end) {
+
+			int less = cache.count[begin];
+			int lessIndex = begin;
+
+			for (int i = begin + 1; i < end; i++) {
+				if (cache.count[i] < less) {
+					less = cache.count[i];
+					lessIndex = i;
+				}
+			}
+
+			return lessIndex;
+
+		}
+
+		int findLeastRecentlyUsed (Cache &cache) {
+
+			int greater = cache.count[0];
+			int greaterIndex = 0;
+			for (int i = 1; i < cache.lines; i++) {
+				if (cache.count[i] > greater) {
+					greater = cache.count[i];
+					greaterIndex = i;
+				}
+			}
+
+			return greaterIndex;
+
+		}
+
+		int findLeastRecentlyUsedSets (Cache &cache, int begin, int end) {
+
+			int greater = cache.count[begin];
+			int greaterIndex = begin;
+
+			for (int i = begin; i < end; i++) {
+				if (cache.count[i] > greater) {
+					greater = cache.count[i];
+					greaterIndex = i;
+				}
+			}
+
+			return greaterIndex;
+
+		}
+
+		void changeLeastRecentlyUsed (Cache &cache) {
+			for (int i = 0; i < cache.lines; i++) {
+				cache.count[i] = cache.count[i] + 1;
+			}
+		}
+
+		void changeLeastRecentlyUsedSets (Cache &cache, int begin, int end) {
+			for (int i = begin + 1; i < end; i++) {
+				cache.count[i] = cache.count[i] + 1;
+			}
+		}
+
+};
+
+	void changeBlocks (Memory &memory, Cache &cache, int blockCache, int blockMemory) {
+		for (int i = 0; memory.wordsPerBlocks; i++) {
+			cache.elements[blockCache][i] = memory.elements[blockMemory][i];
+		}
+	}
+
+	bool findWord (Cache &cache, int word, int &block, int begin, int end) {
+
+		for (int i = begin; i < end; i++) {
+			for (size_t j = 0; j < cache.elements[i].size (); j++) {
+				if (cache.elements[i][j] == word) {
+					block = i;
+					return true;
+				}
+			}
+		}
+
+		return false;
+
+	}
 
 #endif
--- Cache/src/main.cpp
+++ Cache/src/main.cpp
@@ -1,12 +1,420 @@
 #include <iostream>
-#include "./cache.cpp"
+#include <fstream>
+#include <string>
+#include <ctime>
+#include "../include/cache.h"
 
 int main (int argc, char * argv[]) {
 
-	std::string config = "./data/config.txt";
+	/// Variables
+	std::string instruction;
+	std::size_t found;
+	int word = 0, blockCache = 0, blockMemory = 0, rest = 0, 
+	sizeWay = 0, blockWay = 0, begin = 0, end = 0, blockCacheAux = 0;
+	bool state = false;
 
-	Cache cache (config);
+	/// Reads the configuratio file
+	std::ifstream file;
+	file.open ("./data/config.txt");
 
-	cache.showConfig ();
-	
+	/// Sets the parameters
+	Memory memory;
+	Cache cache;
+	file >> memory.wordsPerBlocks >> cache.lines >> memory.memorySize >>
+			memory.mapping >> memory.setSize >> memory.replacementPolicy;
+
+	cache.wordsPerBlocks = memory.wordsPerBlocks;
+
+	/// Creates the objects
+	memory.createMemory (memory);
+	cache.createCache (cache);
+
+	/// Counter for LFU and LRU policy
+	std::vector<int> blockCacheSets (memory.setSize, 0);
+
+	/// Using the mapping informed
+	switch (memory.mapping) {
+		/// Directed-Mapped
+		case 1:
+			/// Gets the commando from the user
+			while (std::getline (std::cin, instruction)) {
+
+				/// Search for a space in the command
+				found = instruction.find (" ");
+				/// If it finds it, splits the command
+				if (found != std::string::npos) {
+					word = std::stoi (instruction.substr (found + 1));
+					instruction = instruction.substr (0, found);
+				}
+
+				/// Reading the command
+				if (instruction.compare ("Read") == 0) {
+
+					blockCache = (word / memory.wordsPerBlocks) % cache.lines;
+					blockMemory = word / memory.wordsPerBlocks;
+					rest = word % memory.wordsPerBlocks;
+
+					if (cache.elements[blockCache][rest] == word) {
+						std::cout << "HIT - Linha " << blockCache << std::endl;
+					} else {
+						std::cout << "MISS - Alocado na linha " << blockCache << " - Bloco " << blockMemory << " substituído." << std::endl;
+						changeBlocks (memory, cache, blockCache, blockMemory);
+					}
+
+				} else if (instruction.compare("Show") == 0) {
+					cache.showCache (cache, memory);
+					memory.showMemory (memory);
+				}
+
+			}
+			break;
+		/// Fully Associative
+		case 2: 
+			switch (memory.replacementPolicy) {
+				/// Random
+				case 1:
+					while (std::getline (std::cin, instruction)) {
+
+						begin = 0;
+						end = cache.elements.size ();
+						/// Gets the command
+						found = instruction.find (" ");
+						if (found != std::string::npos) {
+							word = std::stoi (instruction.substr (found + 1));
+							instruction = instruction.substr (0, found);
+						}
+
+						blockMemory = word / memory.wordsPerBlocks;
+
+						if (instruction.compare ("Read") == 0) {
+
+							/// Search inside the cache
+							state = findWord (cache, word, blockCache, begin, end);
+							if (state) {
+								std::cout << "HIT - Linha " << blockCache << std::endl;
+								state = false;
+							} else { 	//!< Find the first available position in the cache
+								state = findWord (cache, -1, blockCache, begin, end);
+								if (state) {
+									changeBlocks (memory, cache, blockCache, blockMemory);
+								} else {
+									srand (time(NULL));
+									blockCache = rand () % cache.lines;
+									changeBlocks (memory, cache, blockCache, blockMemory);
+								}
+							}
+
+							std::cout << "MISS - Alocado na linha " << blockCache << " - Bloco " << blockMemory << " substituído." << std::endl;
+
+						} else if (instruction.compare ("Show") == 0) {
+							cache.showCache (cache, memory);
+							memory.showMemory (memory);
+						}
+
+					} break;
+				/// FIFO
+				case 2: 
+					while (std::getline (std::cin, instruction)) {
+
+						begin = 0;
+						end = cache.elements.size ();
+						/// Gets the command
+						found = instruction.find (" ");
+						if (found != std::string::npos) {
+							word = std::stoi (instruction.substr (found + 1));
+							instruction = instruction.substr (0, found);
+						}
+
+						blockMemory = word / memory.wordsPerBlocks;
+
+						if (instruction.compare ("Read") == 0) {
+
+							/// Search inside the cache
+							state = findWord (cache, word, blockCache, begin, end);
+							if (state) {
+								std::cout << "HIT - Linha " << blockCache << std::endl;
+								state = false;
+							} else {
+								changeBlocks (memory, cache, blockCacheAux % cache.lines, blockMemory);
+								std::cout << "MISS - Alocado na linha " << (blockCacheAux % cache.lines) << " - Bloco " << blockMemory << " substituído." << std::endl;
+								blockCacheAux = blockCacheAux + 1;
+							}
+
+						} else if (instruction.compare ("Show") == 0) {
+							cache.showCache (cache, memory);
+							memory.showMemory (memory);
+						}
+
+					} break;
+				/// LFU
+				case 3:
+					for (int i = 0; i < cache.lines; i++) {
+						cache.count.push_back(0);
+					}
+					while (std::getline (std::cin, instruction)) {
+
+						begin = 0;
+						end = cache.elements.size ();
+						/// Gets the command
+						found = instruction.find (" ");
+						if (found != std::string::npos) {
+							word = std::stoi (instruction.substr (found + 1));
+							instruction = instruction.substr (0, found);
+						}
+
+						blockMemory = word / memory.wordsPerBlocks;
+
+						if (instruction.compare ("Read") == 0) {
+
+							/// Search inside the cache
+							state = findWord (cache, word, blockCache, begin, end);
+							if (state) {
+								std::cout << "HIT - Linha " << blockCache << std::endl;
+								cache.count[blockCache] = cache.count[blockCache] + 1;
+								state = false;
+							} else {
+								state = findWord (cache, -1, blockCache, begin, end);
+								if (state) {
+									changeBlocks (memory, cache, blockCache, blockMemory);
+									cache.count[blockCache] = 1;
+								} else {
+									blockCache = cache.findLeastFrequentlyUsed (cache);
+									changeBlocks (memory, cache, blockCache, blockMemory);
+									cache.count[blockCache] = 1;
+								}
+								std::cout << "MISS - Alocado na linha " << blockCache << " - Bloco " << blockMemory << " substituído." << std::endl;
+							}
+
+						} else if (instruction.compare ("Show") == 0) {
+							cache.showCache (cache, memory);
+							memory.showMemory (memory);
+						}
+
+					} break;
+				/// LRU
+				case 4:
+					for (int i = 0; i < cache.lines; i++) {
+						cache.count.push_back(0);
+					}
+					while (std::getline (std::cin, instruction)) {
+
+						begin = 0;
+						end = cache.elements.size ();
+						/// Gets the command
+						found = instruction.find (" ");
+						if (found != std::string::npos) {
+							word = std::stoi (instruction.substr (found + 1));
+							instruction = instruction.substr (0, found);
+						}
+
+						blockMemory = word / memory.wordsPerBlocks;
+
+						if (instruction.compare ("Read") == 0) {
+
+							/// Search inside the cache
+							state = findWord (cache, word, blockCache, begin, end);
+							if (state) {
+								std::cout << "HIT - Linha " << blockCache << std::endl;
+								cache.count[blockCache] = -1;
+								state = false;
+							} else {
+								state = findWord (cache, -1, blockCache, begin, end);
+								if (state) {
+									changeBlocks (memory, cache, blockCache, blockMemory);
+									cache.count[blockCache] = -1;
+								} else {
+									blockCache = cache.findLeastRecentlyUsed (cache);
+									changeBlocks (memory, cache, blockCache, blockMemory);
+									cache.count[blockCache] = -1;
+								}
+								std::cout << "MISS - Alocado na linha " << blockCache << " - Bloco " << blockMemory << " substituído." << std::endl;
+							}
+							cache.changeLeastRecentlyUsed (cache);
+						} else if (instruction.compare ("Show") == 0) {
+							cache.showCache (cache, memory);
+							memory.showMemory (memory);
+						}
+
+					} break;
+
+			} break;
+		/// Set Associative
+		case 3:
+			switch (memory.replacementPolicy) {
+				/// Random
+				case 1:
+					while (std::getline (std::cin, instruction)) {
+
+						/// Gets the command
+						found = instruction.find (" ");
+						if (found != std::string::npos) {
+							word = std::stoi (instruction.substr (found + 1));
+							instruction = instruction.substr (0, found);
+						}
+
+						blockMemory = word / memory.wordsPerBlocks;
+						sizeWay = cache.lines / memory.setSize;
+						blockWay = blockMemory % memory.setSize;
+						begin = (blockWay * sizeWay);
+						end = begin + sizeWay;
+
+						if (instruction.compare ("Read") == 0) {
+
+							state = findWord (cache, word, blockCache, begin, end);
+							if (state) {
+								std::cout << "HIT - Linha " << blockCache << std::endl;
+								state = false;
+							} else { 	//!< Find the first available position in the cache
+								state = findWord (cache, -1, blockCache, begin, end);
+								if (state) {
+									changeBlocks (memory, cache, blockCache, blockMemory);
+									state = false;
+								} else {
+									srand (time(NULL));
+									blockCache = rand () % cache.lines;
+									changeBlocks (memory, cache, blockCache, blockMemory);
+								}
+							}
+
+							std::cout << "MISS - Alocado na linha " << blockCache << " - Bloco " << blockMemory << " substituído." << std::endl;
+
+						} else if (instruction.compare ("Show") == 0) {
+							cache.showCache (cache, memory);
+							memory.showMemory (memory);
+						}
+
+					} break;
+				/// FIFO
+				case 2:
+					while (std::getline (std::cin, instruction)) {
+
+						/// Gets the command
+						found = instruction.find (" ");
+						if (found != std::string::npos) {
+							word = std::stoi (instruction.substr (found + 1));
+							instruction = instruction.substr (0, found);
+						}
+
+						blockMemory = word / memory.wordsPerBlocks;
+						sizeWay = cache.lines / memory.setSize;
+						blockWay = blockMemory % memory.setSize;
+						begin = (blockWay * sizeWay);
+						end = begin + sizeWay;
+
+						if (instruction.compare ("Read") == 0) {
+
+							state = findWord (cache, word, blockCache, begin, end);
+							if (state) {
+								std::cout << "HIT - Linha " << blockCache << std::endl;
+								state = false;
+							} else {
+								changeBlocks (memory, cache, (begin + blockCacheSets[blockWay] % sizeWay), blockMemory);
+								std::cout << "MISS - Alocado na linha " << (begin + blockCacheSets[blockWay] % sizeWay) << " - Bloco " << blockMemory << " substituído." << std::endl;
+								blockCacheSets[blockWay] = blockCacheSets[blockWay] + 1;
+							}
+
+						} else if (instruction.compare ("Show") == 0) {
+							cache.showCache (cache, memory);
+							memory.showMemory (memory);
+						}
+
+					} break;
+				/// LFU
+				case 3: 
+					for (int i = 0; i < cache.lines; i++) {
+						cache.count.push_back(0);
+					}
+					while (std::getline (std::cin, instruction)) {
+						// Gets the command
+						found = instruction.find (" ");
+						if (found != std::string::npos) {
+							word = std::stoi (instruction.substr (found + 1));
+							instruction = instruction.substr (0, found);
+						}
+
+						blockMemory = word / memory.wordsPerBlocks;
+						sizeWay = cache.lines / memory.setSize;
+						blockWay = blockMemory % memory.setSize;
+						begin = (blockWay * sizeWay);
+						end = begin + sizeWay;
+
+						if (instruction.compare ("Read") == 0) {
+
+							state = findWord (cache, word, blockCache, begin, end);
+							if (state) {
+								std::cout << "HIT - Linha " << blockCache << std::endl;
+								cache.count[blockCache] = cache.count[blockCache] + 1;
+								state = false;
+							} else {
+								state = findWord (cache, -1, blockCache, begin, end);
+								if (state) {
+									changeBlocks (memory, cache, blockCache, blockMemory);
+									cache.count[blockCache] = 1;
+								} else {
+									blockCache = cache.findLeastFrequentlyUsed (cache);
+									changeBlocks (memory, cache, blockCache, blockMemory);
+									cache.count[blockCache] = 1;
+								}
+								std::cout << "MISS - Alocado na linha " << blockCache << " - Bloco " << blockMemory << " substituído." << std::endl;
+							}
+
+						} else if (instruction.compare ("Show") == 0) {
+							cache.showCache (cache, memory);
+							memory.showMemory (memory);
+						}
+
+					} break;
+				/// LRU
+				case 4: 
+					for (int i = 0; i < cache.lines; i++) {
+						cache.count.push_back(0);
+					}
+					while (std::getline (std::cin, instruction)) {
+						// Gets the command
+						found = instruction.find (" ");
+						if (found != std::string::npos) {
+							word = std::stoi (instruction.substr (found + 1));
+							instruction = instruction.substr (0, found);
+						}
+
+						blockMemory = word / memory.wordsPerBlocks;
+						sizeWay = cache.lines / memory.setSize;
+						blockWay = blockMemory % memory.setSize;
+						begin = (blockWay * sizeWay);
+						end = begin + sizeWay;
+
+						if (instruction.compare ("Read") == 0) {
+
+							/// Search inside the cache
+							state = findWord (cache, word, blockCache, begin, end);
+							if (state) {
+								std::cout << "HIT - Linha " << blockCache << std::endl;
+								cache.count[blockCache] = -1;
+								state = false;
+							} else {
+								state = findWord (cache, -1, blockCache, begin, end);
+								if (state) {
+									changeBlocks (memory, cache, blockCache, blockMemory);
+									cache.count[blockCache] = -1;
+								} else {
+									blockCache = cache.findLeastRecentlyUsedSets (cache, begin, end);
+									changeBlocks (memory, cache, blockCache, blockMemory);
+									cache.count[blockCache] = -1;
+								}
+								std::cout << "MISS - Alocado na linha " << blockCache << " - Bloco " << blockMemory << " substituído." << std::endl;
+							}
+							cache.changeLeastRecentlyUsedSets (cache, begin, end);
+						} else if (instruction.compare ("Show") == 0) {
+							cache.showCache (cache, memory);
+							memory.showMemory (memory);
+						}
+
+					} break;
+			} break;
+
+	}
+
+	file.close ();
+	return 0;
+
 }
